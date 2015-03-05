32af27497944bd605d58445dbe1de69b137e0008
daanlouter <daan.louter@gmail.com>
2015-03-05 18:35:31 +0000

    woof

--- src/js/main.js
+++ src/js/main.js
@@ -12,7 +12,7 @@ define([
 ) {
    'use strict';
 
-    var dogs;
+    var dogs, title, standfirst, byline;
 
     function init(el, context, config, mediator) {
         $.getJSON('http://interactive.guim.co.uk/spreadsheetdata/1j5tKq-oRLIiBP51kgjHRcdf2UxCR1zlukj7v1-6qT-c.json',function(response){
@@ -25,8 +25,8 @@ define([
 
     function renderDogs(el){
         var selectedDogHash = document.location.hash ? document.location.hash.replace('#','') : null;
-
         $('article.content--interactive').addClass('interactiveContainer');
+        getPageData();
 
         dogs.map(function(dog){
             if(selectedDogHash === dog.id){
@@ -34,7 +34,7 @@ define([
             }else{
                 dog.selected = false;
             }
-            dog.story = dog.story.split('\n').filter(function(paragraph){
+            dog.story = dog.story.split('\n').filter(function(paragraph,i){
                 return paragraph;
             })
         })
@@ -42,7 +42,12 @@ define([
             el:el,
             template:appTemplate,
             data: {
-                allDogs: dogs
+                allDogs: dogs,
+                furniture: {
+                    title: title,
+                    standfirst: standfirst,
+                    byline: byline
+                }
             }
         })
 
@@ -66,23 +71,19 @@ define([
 
     function shareContent(event, platform){
         var shareWindow;
-        var twitterBaseUrl = "https://twitter.com/home?status=";
+        var twitterBaseUrl = "http://twitter.com/share?text=";
         var facebookBaseUrl = "https://www.facebook.com/dialog/feed?display=popup&app_id=741666719251986&link=";
-        
-        var sharemessage = event.context.name + " " + event.context.shareline;
+        var sharemessage = "Britain's most heroic dogs: " + event.context.name + ", " + event.context.shareline;
         var urlsuffix = "#" + event.context.id;
         var shareImage = "@@assetPath@@/imgs/" + event.context.image;
-        var guardianUrl = "http://interactive.guim.co.uk/2015/03/hero-dogs/" + urlsuffix;
-        
-        console.log(shareImage);
+        var guardianUrl = "http://gu.com/p/46bdb/" + urlsuffix;
          
         if(platform === "twitter"){
             shareWindow = 
                 twitterBaseUrl + 
                 encodeURIComponent(sharemessage) + 
-                "%20" + 
-                encodeURIComponent(guardianUrl)
-            
+                "&url=" + 
+                encodeURIComponent(guardianUrl)   
         }else if(platform === "facebook"){
             shareWindow = 
                 facebookBaseUrl + 
@@ -97,6 +98,38 @@ define([
                 "&body=" + guardianUrl 
         }
         window.open(shareWindow, platform + "share", "width=640,height=320");
+    }
+
+    function getPageData(){
+        var furniture = {
+            title: "Have cape, will travel",
+            standfirst: "Meet Britain’s most heroic dogs"
+        };
+        
+        // var title = furniture.title;
+        title = $('header.content__head h1');
+        standfirst = $('header.content__head .content__standfirst p');
+        byline = $('.content__main .content__meta-container.u-cf');
+
+
+        if(title.length > 0){
+            title = $(title).get(0).textContent;
+        }else{
+            title = furniture.title;
+        }
+
+        if(standfirst.length > 0){
+            standfirst = $(standfirst).get(0).textContent;
+        }else{
+            standfirst = furniture.standfirst;
+        }
+
+        if(byline.length > 0){
+            byline = $(byline).get(0).outerHTML;
+        }else{
+            byline = "";
+        }
+
     }
 
     return {
